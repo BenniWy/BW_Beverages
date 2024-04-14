c8de114617c24092e92e074c885c21cc9ea35dfc
BenniWy <benjamin.wynants@gmx.de>
2024-04-14 14:02:34 +0200

    Validation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -14,9 +14,7 @@ namespace BW_Beverages.Controllers
         public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
-            Console.WriteLine("+++++UserManager: " +_userManager+ "\n");
             _signInManager = signInManager;
-            Console.WriteLine("+++++SignInManager: " +_signInManager+ "\n");
         }
 
         public IActionResult Login(string returnUrl)
@@ -35,7 +33,6 @@ namespace BW_Beverages.Controllers
                 return View(loginViewModel);
 
             var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
-
             if (user != null)
             {
                 var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
@@ -58,16 +55,10 @@ namespace BW_Beverages.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
-            Console.WriteLine("\n+++++ ModelState valid?: " + ModelState.IsValid +" +++++\n");
-            Console.WriteLine("\n+++++ ModelState: " + ModelState+" +++++\n");
-            Console.WriteLine("\n+++++ registerViewModel: " +registerViewModel+" +++++\n");
             if (ModelState.IsValid)
             {
                 var user = new IdentityUser { UserName = registerViewModel.UserName };
-                Console.WriteLine("\n+++++ user: " +user+" +++++\n");
                 var result = await _userManager.CreateAsync(user, registerViewModel.Password);
-                Console.WriteLine("\n+++++ result: " +result+" +++++\n");
-                Console.WriteLine("\n+++++ result.Succeeded?: " +result.Succeeded+" +++++\n");
 
                 if (result.Succeeded)
                 {
